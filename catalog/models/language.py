2c4c5ad49ecc2fa81d914f722c08687d004c8e91
Daniel <dannyking07@outlook.es>
2023-06-16 10:54:14 -0500

    feat [catalog/models -> language]

---
+++ catalog/models/language.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.urls import reverse
+
+class Language(models.Model):
+    name = models.CharField(max_length=35)
+
+    def str(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("language_detail", args=[str(self.id)])
